8e8b359317a43eeb2778de73001742e97aff587d
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-05-15 15:10:04 +0200

    Init. Empty webapi.

---
+++ Presentation/Services/IInvoiceService.cs
@@ -0,0 +1,5 @@
+namespace Presentation.Services;
+
+public interface IInvoiceService
+{
+}
---
+++ Presentation/Services/InvoiceService.cs
@@ -0,0 +1,6 @@
+namespace Presentation.Services;
+
+public class InvoiceService : IInvoiceService
+{
+    
+}
